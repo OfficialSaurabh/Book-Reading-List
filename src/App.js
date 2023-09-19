050a2c245d3c07efcc66c4791b8e9784ba470708
SAURABH KUMAR <hmsaurabhkr@gmail.com>
2023-09-20 01:00:56 +0530

    added crud opreation using database

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import BookCreate from "./component/BookCreate";
 import BookList from "./component/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
+  const fetchBook = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBook();
+  });
   // Adding Books
-  const createBook = title => {
-    const addedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 1000),
-        title: title,
-      },
-    ];
+  const createBook = async title => {
+    const response = await axios.post("http://localhost:3001/books", { title });
+    const addedBooks = [...books, response.data];
     setBooks(addedBooks);
   };
 
   // Edit Books
-  const editBook = (id, newtitle) => {
-    const editedBooks = books.map((book)=>{
-      if(book.id === id){
-        return {...book, title: newtitle}
+  const editBook = async (id, newtitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newtitle,
+    });
+    const editedBooks = books.map(book => {
+      if (book.id === id) {
+        return { ...book, ...response.data };
       }
       return book;
     });
     setBooks(editedBooks);
-  }
-  
+  };
+
   // Deleting Books
-  const deleteBook = id => {
+  const deleteBook = async id => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const deletBooks = books.filter(book => {
       return book.id !== id;
     });
