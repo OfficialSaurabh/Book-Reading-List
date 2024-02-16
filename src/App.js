aa60bf6fac66a6a1e2bddfaab18dd1ebc6b9fea6
officialsaurabh <hmsaurabhkr@gmail.com>
2024-02-16 07:39:09 +0530

    updated json-server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import BookCreate from "./component/BookCreate";
 import BookList from "./component/BookList";
@@ -6,48 +6,56 @@ import BookList from "./component/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
-  const fetchBook = async () => {
+  const fetchBooks = async () => {
     const response = await axios.get("http://localhost:3001/books");
+
     setBooks(response.data);
   };
 
   useEffect(() => {
-    fetchBook();
-  });
-  // Adding Books
-  const createBook = async title => {
-    const response = await axios.post("http://localhost:3001/books", { title });
-    const addedBooks = [...books, response.data];
-    setBooks(addedBooks);
-  };
+    fetchBooks();
+  }, []);
 
-  // Edit Books
-  const editBook = async (id, newtitle) => {
+  const editBookById = async (id, newTitle) => {
     const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newtitle,
+      title: newTitle,
     });
-    const editedBooks = books.map(book => {
+
+    const updatedBooks = books.map(book => {
       if (book.id === id) {
         return { ...book, ...response.data };
       }
+
       return book;
     });
-    setBooks(editedBooks);
+
+    setBooks(updatedBooks);
   };
 
-  // Deleting Books
-  const deleteBook = async id => {
+  const deleteBookById = async id => {
     await axios.delete(`http://localhost:3001/books/${id}`);
-    const deletBooks = books.filter(book => {
+
+    const updatedBooks = books.filter(book => {
       return book.id !== id;
     });
-    setBooks(deletBooks);
+
+    setBooks(updatedBooks);
+  };
+
+  const createBook = async title => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
+    setBooks(updatedBooks);
   };
 
   return (
-    <div className="App">
+    <div className="app">
+      <h1>Reading List</h1>
       <BookCreate onCreate={createBook} />
-      <BookList books={books} onDelete={deleteBook} onEdit={editBook} />
+      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
     </div>
   );
 }
--- src/component/BookShow.js
+++ src/component/BookShow.js
@@ -1,36 +1,39 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import BookEdit from "./BookEdit";
 
 function BookShow({ book, onDelete, onEdit }) {
   const [showEdit, setShowEdit] = useState(false);
 
-  const handelEdit = () => {
-    setShowEdit(!showEdit);
-  };
-
-  const handelDelete = () => {
+  const handleDeleteClick = () => {
     onDelete(book.id);
   };
 
-  const handleSubmit = (id, newtitle) => {
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleSubmit = (id, newTitle) => {
     setShowEdit(false);
-    onEdit(book.id, newtitle);
+    onEdit(id, newTitle);
   };
 
   let content = <h3>{book.title}</h3>;
   if (showEdit) {
-    content = <BookEdit book={book} onSubmit={handleSubmit} />;
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
   }
+
   return (
-    <div
-    style={{
-      margin: "10px"
-    }}
-    >
-      <img src={`https://picsum.photos/seed/${book.id}/200`} alt="" />
-      {content}
-      <button onClick={handelEdit}> Edit</button>
-      <button onClick={handelDelete}>X</button>
+    <div className="book-show" style={{ padding: "10px" }}>
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
